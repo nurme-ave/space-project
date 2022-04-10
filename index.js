00d3c15db155b9bee459a2f3bc61f3257c12c0d3
Ave Nurme <nurme.ave@gmail.com>
2022-04-10 18:38:23 +0300

    Refactor JS

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+const checkboxText = document.querySelector('.checkbox-consent');
 let formEl = document.querySelector('.form');
+
 formEl.addEventListener('submit', (e) => {
   e.preventDefault();
-  console.log(e.target);
 
   let formData = new FormData(e.target);
   let userFirstName = formData.get('first-name');
-  let capitalizedFirstName = userFirstName.charAt(0).toUpperCase() + userFirstName.slice(1).toLowerCase()
+  let capitalizedFirstName =
+    userFirstName.charAt(0).toUpperCase() +
+    userFirstName.slice(1).toLowerCase();
   let userEmailAddress = formData.get('email-address');
 
   const firstCheckbox = document.getElementById('consent').checked;
@@ -16,21 +19,20 @@ formEl.addEventListener('submit', (e) => {
       <p class="info-text-2">We'll be in contact within 24 hours and send a confirmation to ${userEmailAddress}!</p>
       <button id="go-back" class="submit-button">Go back</button>
       `;
-  
+
     let cardContent = document.getElementById('card-content');
     cardContent.innerHTML = updatedHTMLContent;
-  
+
     document.getElementById('go-back').addEventListener('click', () => {
       location.reload();
     });
   } else {
-    document.querySelector('.checkbox-consent').style.color = 'red';
+    checkboxText.style.color = 'red';
   }
 });
 
-document.querySelector('.checkbox-consent').addEventListener('change', (e) => {
+checkboxText.addEventListener('change', (e) => {
   if (e.target.checked) {
-    document.querySelector('.checkbox-consent').style.color = 'white';
+    checkboxText.style.color = 'white';
   }
-
 });
