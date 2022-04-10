88aee0978403515954815d4e0120733f041e86cd
Ave Nurme <nurme.ave@gmail.com>
2022-04-10 19:28:38 +0300

    Add new font-weight in HTML, tweak .accent

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ formEl.addEventListener('submit', (e) => {
   if (firstCheckbox) {
     let updatedHTMLContent = `
       <h2 class="updated-content">Thank you, ${capitalizedFirstName}, your form was submitted successfully!</h2>
-      <p class="info-text-2">We'll be in contact within 24 hours and send a confirmation to ${userEmailAddress}!</p>
+      <p class="info-text-2">We'll be in contact within 24 hours and send a confirmation to <span class="accent">${userEmailAddress}!</span></p>
       <button id="go-back" class="submit-button">Go back</button>
       `;
 
