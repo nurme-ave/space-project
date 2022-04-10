977e4fa94c1170bdb3e5ec1bc2e9dbc14c01027f
Ave Nurme <nurme.ave@gmail.com>
2022-04-10 13:58:00 +0300

    Add event listeners to the consent checkbox and make sure it's checked

--- index.js
+++ index.js
@@ -7,16 +7,29 @@ formEl.addEventListener('submit', (e) => {
   let userFirstName = formData.get('first-name');
   let userEmailAddress = formData.get('email-address');
 
-  let updatedHTMLContent = `
-    <h2 class="updated-content">Thank you, ${userFirstName}, your form was submitted successfully!</h2>
-    <p class="be-in-contact">We'll be in contact within 24 hours and send a confirmation to ${userEmailAddress}!</p>
-    <button id="go-back" class="submit-button">Go back</button>
-    `;
+  const firstCheckbox = document.getElementById('consent').checked;
 
-  let cardContent = document.getElementById('card-content');
-  cardContent.innerHTML = updatedHTMLContent;
+  if (firstCheckbox) {
+    let updatedHTMLContent = `
+      <h2 class="updated-content">Thank you, ${userFirstName}, your form was submitted successfully!</h2>
+      <p class="be-in-contact">We'll be in contact within 24 hours and send a confirmation to ${userEmailAddress}!</p>
+      <button id="go-back" class="submit-button">Go back</button>
+      `;
+  
+    let cardContent = document.getElementById('card-content');
+    cardContent.innerHTML = updatedHTMLContent;
+  
+    document.getElementById('go-back').addEventListener('click', () => {
+      location.reload();
+    });
+  } else {
+    document.querySelector('.cb1').style.color = 'red';
+  }
+});
 
-  document.getElementById('go-back').addEventListener('click', () => {
-    location.reload();
-  });
+document.querySelector('.cb1').addEventListener('change', (e) => {
+  if (e.target.checked) {
+    document.querySelector('.cb1').style.color = 'white';
+  }
+
 });
