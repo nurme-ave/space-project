c749ad12839a24659331f1d2111c6d7104b68a1e
Ave Nurme <nurme.ave@gmail.com>
2022-04-10 13:16:32 +0300

    Make the app mobile-friendly

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 let formEl = document.querySelector('.form');
 formEl.addEventListener('submit', (e) => {
   e.preventDefault();
-  console.log(e.target)
+  console.log(e.target);
 
   let formData = new FormData(e.target);
   let userFirstName = formData.get('first-name');
+  let userEmailAddress = formData.get('email-address');
 
   let updatedHTMLContent = `
     <h2 class="updated-content">Thank you, ${userFirstName}, your form was submitted successfully!</h2>
-    <p class="be-in-contact">We'll be in contact within 24 hours!</p>
+    <p class="be-in-contact">We'll be in contact within 24 hours and send a confirmation to ${userEmailAddress}!</p>
+    <button id="go-back" class="submit-button">Go back</button>
     `;
+
   let cardContent = document.getElementById('card-content');
   cardContent.innerHTML = updatedHTMLContent;
+
+  document.getElementById('go-back').addEventListener('click', () => {
+    location.reload();
+  });
 });
